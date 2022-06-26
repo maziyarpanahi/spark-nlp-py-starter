6cf8d7d75dee17b2a4ca52b3fcef4a391204a1ea
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2022-06-26 18:12:13 +0200

    Update 0_basics.py

--- test/0_basics.py
+++ test/0_basics.py
@@ -10,6 +10,28 @@ spark = sparknlp.start()
 text_list = ['Peter Parker is a nice guy and lives in New York.',
              'Bruce Wayne is also a nice guy and lives in Gotham City.']
 
+long_text = '''google is acquiring data science community kaggle. Sources tell us that google is acquiring kaggle, 
+a platform that hosts data science and machine learning competitions. Details about the transaction remain somewhat 
+vague , but given that google is hosting its Cloud Next conference in san francisco this week, the official 
+announcement could come as early as tomorrow. Reached by phone, kaggle co-founder ceo anthony goldbloom declined to 
+deny that the acquisition is happening. google itself declined 'to comment on rumors'. kaggle, which has about half a 
+million data scientists on its platform, was founded by Goldbloom and Ben Hamner in 2010. The service got an early 
+start and even though it has a few competitors like DrivenData, TopCoder and HackerRank, it has managed to stay well 
+ahead of them by focusing on its specific niche. The service is basically the de facto home for running data science 
+and machine learning competitions. With kaggle, google is buying one of the largest and most active communities for 
+data scientists - and with that, it will get increased mindshare in this community, too (though it already has plenty 
+of that thanks to Tensorflow and other projects). kaggle has a bit of a history with google, too, but that's pretty 
+recent. Earlier this month, google and kaggle teamed up to host a $100,000 machine learning competition around 
+classifying YouTube videos. That competition had some deep integrations with the google Cloud platform, 
+too. Our understanding is that google will keep the service running - likely under its current name. While the 
+acquisition is probably more about Kaggle's community than technology, kaggle did build some interesting tools for 
+hosting its competition and 'kernels', too. On kaggle, kernels are basically the source code for analyzing data sets 
+and developers can share this code on the platform (the company previously called them 'scripts'). Like similar 
+competition-centric sites, kaggle also runs a job board, too. It's unclear what google will do with that part of the 
+service. According to Crunchbase, kaggle raised $12.5 million (though PitchBook says it's $12.75) since its launch in 
+2010. Investors in kaggle include Index Ventures, SV Angel, Max Levchin, Naval Ravikant, google chief economist Hal 
+Varian, Khosla Ventures and Yuri Milner '''
+
 df = spark.createDataFrame(text_list, StringType()).toDF("text")
 
 entities = ['Wall Street', 'USD', 'stock', 'NYSE']
@@ -146,5 +168,25 @@ def test_dep_parsers():
     pipeline_model.transform(df).collect()
     light_model = LightPipeline(pipeline_model)
     light_model.annotate(text_list)
+
+    assert True
+
+
+def test_yake_extraction():
+    keywords = YakeKeywordExtraction() \
+        .setInputCols("token") \
+        .setOutputCol("keywords") \
+        .setMinNGrams(1) \
+        .setMaxNGrams(3) \
+        .setNKeywords(20) \
+        .setStopWords(stopwords)
+
+    yake_pipeline = Pipeline(stages=[documentAssembler, sentenceDetector, toke, keywords])
+
+    empty_df = spark.createDataFrame([['']]).toDF("text")
+
+    yake_Model = yake_pipeline.fit(empty_df)
+    light_model = LightPipeline(yake_Model)
+    light_model.fullAnnotate(text)
 
     assert True
