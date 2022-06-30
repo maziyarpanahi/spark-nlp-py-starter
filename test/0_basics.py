4680878b807f1e9b321b6245a675c259a9a16960
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2022-06-30 10:06:26 +0200

    Update 0_basics.py

--- test/0_basics.py
+++ test/0_basics.py
@@ -58,142 +58,142 @@ toke = Tokenizer() \
     .setOutputCol("token")
 
 
-# @pytest.mark.fast
-# def test_preprocessing_pipeline():
-#     norm = Normalizer() \
-#         .setInputCols("token") \
-#         .setOutputCol("normalized") \
-#         .setLowercase(True) \
-#         .setCleanupPatterns(["[^\w\d\s]"]) \
-#         .setSlangDictionary("./resources/slangs.txt", ",")
-#
-#     stopwords_cleaner = StopWordsCleaner() \
-#         .setInputCols("normalized") \
-#         .setOutputCol("cleanTokens") \
-#         .setStopWords(["is", "the", "a"]) \
-#         .setCaseSensitive(False)
-#
-#     lemmatizer = Lemmatizer() \
-#         .setInputCols("token") \
-#         .setOutputCol("lemma") \
-#         .setDictionary("./resources/AntBNC_lemmas_ver_001.txt", value_delimiter="\t", key_delimiter="->")
-#
-#     pos_tagger = PerceptronModel.pretrained() \
-#         .setInputCols("document", "token") \
-#         .setOutputCol("pos")
-#
-#     ngrams_cum = NGramGenerator() \
-#         .setInputCols("token") \
-#         .setOutputCol("ngrams") \
-#         .setN(3) \
-#         .setEnableCumulative(True) \
-#         .setDelimiter("_")  # Default is space
-#
-#     nlpPipeline = Pipeline(stages=[
-#         documentAssembler,
-#         sentenceDetector,
-#         toke,
-#         norm,
-#         stopwords_cleaner,
-#         lemmatizer,
-#         pos_tagger,
-#         ngrams_cum
-#     ])
-#
-#     pipelineModel = nlpPipeline.fit(df)
-#     pipelineModel.transform(df).collect()
-#     assert True
-#
-#
-# @pytest.mark.slow
-# def test_graph_extraction():
-#     tokenizer = Tokenizer() \
-#         .setInputCols("document") \
-#         .setOutputCol("token")
-#
-#     word_embeddings = WordEmbeddingsModel.pretrained() \
-#         .setInputCols("document", "token") \
-#         .setOutputCol("embeddings")
-#
-#     ner_tagger = NerDLModel.pretrained() \
-#         .setInputCols("document", "token", "embeddings") \
-#         .setOutputCol("ner")
-#
-#     # MergeEntities(True) Pos, Dependency Parser and Typed Dependency Parser features under the hood
-#     graph_extraction = GraphExtraction() \
-#         .setInputCols("document", "token", "ner") \
-#         .setOutputCol("graph") \
-#         .setRelationshipTypes(["lad-PER", "lad-LOC"]) \
-#         .setMergeEntities(True)
-#
-#     graph_pipeline = Pipeline().setStages([
-#         documentAssembler,
-#         tokenizer,
-#         word_embeddings,
-#         ner_tagger,
-#         graph_extraction])
-#
-#     # The result dataset has a graph column with the paths between prefer,LOC relationship
-#     graph_data_set = graph_pipeline.fit(df).transform(df)
-#     graph_data_set.select("graph").collect()
-#
-#     assert True
-#
-#
-# @pytest.mark.fast
-# def test_dep_parsers():
-#     tokenizer = Tokenizer() \
-#         .setInputCols("document") \
-#         .setOutputCol("token")
-#
-#     pos_tagger = PerceptronModel.pretrained() \
-#         .setInputCols("document", "token") \
-#         .setOutputCol("pos")
-#
-#     dep_parser = DependencyParserModel.pretrained() \
-#         .setInputCols("document", "pos", "token") \
-#         .setOutputCol("dependency")
-#
-#     typed_dep_parser = TypedDependencyParserModel.pretrained() \
-#         .setInputCols("token", "pos", "dependency") \
-#         .setOutputCol("dependency_type")
-#
-#     dep_parser_pipeline = Pipeline(stages=[
-#         documentAssembler,
-#         tokenizer,
-#         pos_tagger,
-#         dep_parser,
-#         typed_dep_parser])
-#
-#     pipeline_model = dep_parser_pipeline.fit(df)
-#     pipeline_model.transform(df).collect()
-#     light_model = LightPipeline(pipeline_model)
-#     light_model.annotate(text_list)
-#
-#     assert True
-#
-#
-# @pytest.mark.fast
-# def test_yake_extraction():
-#     stopwords = StopWordsCleaner().getStopWords()
-#
-#     keywords = YakeKeywordExtraction() \
-#         .setInputCols("token") \
-#         .setOutputCol("keywords") \
-#         .setMinNGrams(1) \
-#         .setMaxNGrams(3) \
-#         .setNKeywords(20) \
-#         .setStopWords(stopwords)
-#
-#     yake_pipeline = Pipeline(stages=[documentAssembler, sentenceDetector, toke, keywords])
-#
-#     empty_df = spark.createDataFrame([['']]).toDF("text")
-#
-#     yake_Model = yake_pipeline.fit(empty_df)
-#     light_model = LightPipeline(yake_Model)
-#     light_model.fullAnnotate(long_text)
-#
-#     assert True
+@pytest.mark.fast
+def test_preprocessing_pipeline():
+    norm = Normalizer() \
+        .setInputCols("token") \
+        .setOutputCol("normalized") \
+        .setLowercase(True) \
+        .setCleanupPatterns(["[^\w\d\s]"]) \
+        .setSlangDictionary("./resources/slangs.txt", ",")
+
+    stopwords_cleaner = StopWordsCleaner() \
+        .setInputCols("normalized") \
+        .setOutputCol("cleanTokens") \
+        .setStopWords(["is", "the", "a"]) \
+        .setCaseSensitive(False)
+
+    lemmatizer = Lemmatizer() \
+        .setInputCols("token") \
+        .setOutputCol("lemma") \
+        .setDictionary("./resources/AntBNC_lemmas_ver_001.txt", value_delimiter="\t", key_delimiter="->")
+
+    pos_tagger = PerceptronModel.pretrained() \
+        .setInputCols("document", "token") \
+        .setOutputCol("pos")
+
+    ngrams_cum = NGramGenerator() \
+        .setInputCols("token") \
+        .setOutputCol("ngrams") \
+        .setN(3) \
+        .setEnableCumulative(True) \
+        .setDelimiter("_")  # Default is space
+
+    nlpPipeline = Pipeline(stages=[
+        documentAssembler,
+        sentenceDetector,
+        toke,
+        norm,
+        stopwords_cleaner,
+        lemmatizer,
+        pos_tagger,
+        ngrams_cum
+    ])
+
+    pipelineModel = nlpPipeline.fit(df)
+    pipelineModel.transform(df).collect()
+    assert True
+
+
+@pytest.mark.slow
+def test_graph_extraction():
+    tokenizer = Tokenizer() \
+        .setInputCols("document") \
+        .setOutputCol("token")
+
+    word_embeddings = WordEmbeddingsModel.pretrained() \
+        .setInputCols("document", "token") \
+        .setOutputCol("embeddings")
+
+    ner_tagger = NerDLModel.pretrained() \
+        .setInputCols("document", "token", "embeddings") \
+        .setOutputCol("ner")
+
+    # MergeEntities(True) Pos, Dependency Parser and Typed Dependency Parser features under the hood
+    graph_extraction = GraphExtraction() \
+        .setInputCols("document", "token", "ner") \
+        .setOutputCol("graph") \
+        .setRelationshipTypes(["lad-PER", "lad-LOC"]) \
+        .setMergeEntities(True)
+
+    graph_pipeline = Pipeline().setStages([
+        documentAssembler,
+        tokenizer,
+        word_embeddings,
+        ner_tagger,
+        graph_extraction])
+
+    # The result dataset has a graph column with the paths between prefer,LOC relationship
+    graph_data_set = graph_pipeline.fit(df).transform(df)
+    graph_data_set.select("graph").collect()
+
+    assert True
+
+
+@pytest.mark.fast
+def test_dep_parsers():
+    tokenizer = Tokenizer() \
+        .setInputCols("document") \
+        .setOutputCol("token")
+
+    pos_tagger = PerceptronModel.pretrained() \
+        .setInputCols("document", "token") \
+        .setOutputCol("pos")
+
+    dep_parser = DependencyParserModel.pretrained() \
+        .setInputCols("document", "pos", "token") \
+        .setOutputCol("dependency")
+
+    typed_dep_parser = TypedDependencyParserModel.pretrained() \
+        .setInputCols("token", "pos", "dependency") \
+        .setOutputCol("dependency_type")
+
+    dep_parser_pipeline = Pipeline(stages=[
+        documentAssembler,
+        tokenizer,
+        pos_tagger,
+        dep_parser,
+        typed_dep_parser])
+
+    pipeline_model = dep_parser_pipeline.fit(df)
+    pipeline_model.transform(df).collect()
+    light_model = LightPipeline(pipeline_model)
+    light_model.annotate(text_list)
+
+    assert True
+
+
+@pytest.mark.fast
+def test_yake_extraction():
+    stopwords = StopWordsCleaner().getStopWords()
+
+    keywords = YakeKeywordExtraction() \
+        .setInputCols("token") \
+        .setOutputCol("keywords") \
+        .setMinNGrams(1) \
+        .setMaxNGrams(3) \
+        .setNKeywords(20) \
+        .setStopWords(stopwords)
+
+    yake_pipeline = Pipeline(stages=[documentAssembler, sentenceDetector, toke, keywords])
+
+    empty_df = spark.createDataFrame([['']]).toDF("text")
+
+    yake_Model = yake_pipeline.fit(empty_df)
+    light_model = LightPipeline(yake_Model)
+    light_model.fullAnnotate(long_text)
+
+    assert True
 
 
 @pytest.mark.fast
